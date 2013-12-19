bac1e61744b629a27cfd19b278ac8db5385d0320
Simon Wheatley <simonw@codeforthepeople.com>
2013-12-19 23:42:47 +0000

    Convert command seems to work.

--- wp-cli.php
+++ wp-cli.php
@@ -35,7 +35,32 @@ class ENGG_Command extends WP_CLI_Command {
 	 * 
 	 */
 	public function convert() {
+		WP_CLI::log( sprintf( 'Processing %d posts with NextGen Gallery shortcodes', Escape_NextGen_Gallery::init()->count() ) );
+		set_time_limit( 0 );
 
+		$uploads = wp_upload_dir();
+		$baseurl = $uploads['baseurl'];
+
+		$post_ids = Escape_NextGen_Gallery::init()->get_post_ids();
+		
+		foreach ( $post_ids as $post_id ) {
+			Escape_NextGen_Gallery::init()->process_post( $post_id );
+			break;
+		}
+
+		foreach ( Escape_NextGen_Gallery::init()->infos as $info )
+			WP_CLI::log( $info );
+
+		foreach ( Escape_NextGen_Gallery::init()->warnings as $warning )
+			WP_CLI::warning( $warning );
+
+
+		$lines = array(
+			(object) array( 'Converted' => 'posts converted', 'Count' => Escape_NextGen_Gallery::init()->posts_count ),
+			(object) array( 'Converted' => 'images converted', 'Count' => Escape_NextGen_Gallery::init()->images_count ),
+		);
+		$fields = array( 'Converted', 'Count' );
+		\WP_CLI\Utils\format_items( 'table', $lines, $fields );
 	}
 
 }
