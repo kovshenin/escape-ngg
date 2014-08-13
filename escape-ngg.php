8cb6d385b7bee6ca90ec92c87411bdf70b744346
Tim Yale <yalet@lafayette.edu>
2014-08-13 08:59:58 -0400

    Added support for multiple galleries on a page.

--- escape-ngg.php
+++ escape-ngg.php
@@ -6,15 +6,15 @@
  * License: GPLv3
  * Version: 1.0
  *
- * This plugin will scan through all your posts and pages for the [nggallery] shortcode. 
- * It will loop through all images associated with that gallery and recreate them as native 
- * WordPress attachments instead. Finally it will replace the [nggallery] shortcode with 
+ * This plugin will scan through all your posts and pages for the [nggallery] shortcode.
+ * It will loop through all images associated with that gallery and recreate them as native
+ * WordPress attachments instead. Finally it will replace the [nggallery] shortcode with
  * the [gallery] shortcode native to WordPress.
  *
  * Instructions: Backup! Activate the plugin and browse to yourdomain.com/wp-admin/?escape_ngg_please=1
  * When you're done you can delete the gallery dir, and the wp_ngg_* tables in your database. Keep the backups though.
  *
- * Limitations: 
+ * Limitations:
  * - doesn't work with shortcodes other than [nggallery]
  * - doesn't work when more than one gallery on page
  *
@@ -26,9 +26,9 @@ if ( defined( 'WP_CLI' ) && WP_CLI )
 
 
 /**
- * 
- * 
- * @package 
+ *
+ *
+ * @package
  **/
 class Escape_NextGen_Gallery {
 
@@ -62,9 +62,9 @@ class Escape_NextGen_Gallery {
 
 	/**
 	 * Singleton stuff.
-	 * 
+	 *
 	 * @access @static
-	 * 
+	 *
 	 * @return Escape_NextGen_Gallery object
 	 */
 	static public function init() {
@@ -116,7 +116,7 @@ class Escape_NextGen_Gallery {
 		set_time_limit( 600 );
 
 		$post_ids = $this->get_post_ids( $limit );
-		
+
 		foreach ( $post_ids as $post_id ) {
 			$this->process_post( $post_id );
 		}
@@ -136,10 +136,10 @@ class Escape_NextGen_Gallery {
 	// =========
 
 	/**
-	 * 
 	 *
 	 *
-	 * @return int A count of posts with 
+	 *
+	 * @return int A count of posts with
 	 * @author Simon Wheatley
 	 **/
 	public function count() {
@@ -158,98 +158,90 @@ class Escape_NextGen_Gallery {
 		$post = get_post( $post_id );
 		$matches = null;
 
-		preg_match( '#nggallery id(\s)*="?(\s)*(?P<id>\d+)#i', $post->post_content, $matches );
-		if ( ! isset( $matches['id'] ) ) {
+		preg_match_all( '#nggallery id(\s)*="?(\s)*(?P<id>\d+)#i', $post->post_content, $matches, PREG_SET_ORDER );
+		if ( ! isset( $matches[0]['id'] ) ) {
 			$this->warnings[] = sprintf( "Could not match gallery id in %d", $post->ID );
 			return;
 		}
 
-		// If there are existing images attached the post, 
-		// let's remember to exclude them from our new gallery.
-		$existing_attachments_ids = get_posts( array(
-			'post_type' => 'attachment',
-			'post_status' => 'inherit',
-			'post_parent' => $post->ID,
-			'post_mime_type' => 'image',
-			'fields' => 'ids',
-		) );
+		foreach ($matches as $match) {
+			$gallery_id = $match['id'];
+			$path = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}ngg_gallery WHERE gid = ". intval( $gallery_id ), ARRAY_A);
+			$images = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}ngg_pictures WHERE galleryid = ". intval( $gallery_id ) . " ORDER BY sortorder, pid ASC" );
 
-		$gallery_id = $matches['id'];
-		$path = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}ngg_gallery WHERE gid = ". intval( $gallery_id ), ARRAY_A  );
-		$images = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}ngg_pictures WHERE galleryid = ". intval( $gallery_id ) . " ORDER BY sortorder, pid ASC" );
+			if ( ! $path || ! $images ) {
+				$this->warnings[] = sprintf( "Could not find images for nggallery %d", $gallery_id );
+				return;
+			}
+			$attachments_ids = array();
+			foreach ( $images as $image ) {
+				$url = home_url( trailingslashit( $path['path'] ) . $image->filename );
+				$url = apply_filters( 'engg_image_url', $url, $path['path'], $image->filename );
 
-		if ( ! $path || ! $images ) {
-			$this->warnings[] = sprintf( "Could not find images for nggallery %d", $gallery_id );
-			return;
-		}
 
-		foreach ( $images as $image ) {
-			$url = home_url( trailingslashit( $path['path'] ) . $image->filename );
-			$url = apply_filters( 'engg_image_url', $url, $path['path'], $image->filename );
-			
+				// Let's use a hash trick here to find our attachment post after it's been sideloaded.
+				$hash = md5( 'attachment-hash' . $url . $image->description . time() . rand( 1, 999 ) );
 
-			// Let's use a hash trick here to find our attachment post after it's been sideloaded.
-			$hash = md5( 'attachment-hash' . $url . $image->description . time() . rand( 1, 999 ) );
-
-			$result = media_sideload_image( $url, $post->ID, $hash );
-			if ( is_wp_error( $result ) ) {
-				$this->warnings[] = sprintf( "Error loading %s: %s", $url, $result->get_error_message() );
-				continue;
-			} else {
-				$attachments = get_posts( array(
-					'post_parent' => $post->ID,
-					's' => $hash,
-					'post_type' => 'attachment',
-					'posts_per_page' => -1,
-				) );
-
-				if ( ! $attachments || ! is_array( $attachments ) || count( $attachments ) != 1 ) {
-					$this->warnings[] = sprintf( "Could not insert attachment for %d", $post->ID );
+				$result = media_sideload_image( $url, $post->ID, $hash );
+				if ( is_wp_error( $result ) ) {
+					$this->warnings[] = sprintf( "Error loading %s: %s", $url, $result->get_error_message() );
 					continue;
+				} else {
+					$attachments = get_posts( array(
+						'post_parent' => $post->ID,
+						's' => $hash,
+						'post_type' => 'attachment',
+						'posts_per_page' => -1,
+					) );
+
+					if ( ! $attachments || ! is_array( $attachments ) || count( $attachments ) != 1 ) {
+						$this->warnings[] = sprintf( "Could not insert attachment for %d", $post->ID );
+						continue;
+					}
 				}
+
+				// Titles should fallback to the filename.
+				if ( ! trim( $image->alttext ) ) {
+					$image->alttext = $image->filename;
+				}
+
+				$attachment = $attachments[0];
+				$attachment->post_title = $image->alttext;
+				$attachment->post_content = $image->description;
+				$attachment->menu_order = $image->sortorder;
+
+				update_post_meta( $attachment->ID, '_wp_attachment_image_alt', $image->alttext );
+				$attachments_ids[] = $attachment->ID;
+
+				wp_update_post( $attachment );
+				$this->images_count++;
+				$this->infos[] = sprintf( "Added attachment for %d", $post->ID );
 			}
 
-			// Titles should fallback to the filename.
-			if ( ! trim( $image->alttext ) ) {
-				$image->alttext = $image->filename;
+			if ( 0 == $this->images_count ) {
+				$this->warnings[] = sprintf( "Could not load images for nggallery %d", $gallery_id );
+				return;
 			}
 
-			$attachment = $attachments[0];
-			$attachment->post_title = $image->alttext;
-			$attachment->post_content = $image->description;
-			$attachment->menu_order = $image->sortorder;
+			// Construct the [gallery] shortcode
+			$attr = array();
+			$attr['ids'] = implode( ',', $attachments_ids );
 
-			update_post_meta( $attachment->ID, '_wp_attachment_image_alt', $image->alttext );
-
-			wp_update_post( $attachment );
-			$this->images_count++;
-			$this->infos[] = sprintf( "Added attachment for %d", $post->ID );
+			$gallery = '[gallery ';
+			foreach ( $attr as $key => $value ) {
+				$gallery .= sprintf( ' %s="%s"', esc_attr( $key ), esc_attr( $value ) );
+				$gallery .= ']';
+			}
+			// Booyaga!
+			$pristine_content = $post->post_content;
+			$post->post_content = preg_replace( "#\[nggallery id(\s)*=\"?(\s)*($gallery_id)(\s)*\]#i", $gallery, $post->post_content );
+			$post->post_content = apply_filters( 'engg_post_content', $post->post_content, $pristine_content, $attr, $post, $gallery );
+			wp_update_post( $post );
+			$this->infos[] = sprintf( "Updated post %d", $post->ID );
 		}
 
-		if ( 0 == $this->images_count ) {
-			$this->warnings[] = sprintf( "Could not load images for nggallery %d", $gallery_id );
-			return;
-		}
-
-		// Construct the [gallery] shortcode
-		$attr = array();
-		if ( $existing_attachments_ids )
-			$attr['exclude'] = implode( ',', $existing_attachments_ids );
-
-		$gallery = '[gallery';
-		foreach ( $attr as $key => $value )
-			$gallery .= sprintf( ' %s="%s"', esc_attr( $key ), esc_attr( $value ) );
-		$gallery .= ']';
-
-		// Booyaga!
-		$pristine_content = $post->post_content;
-		$post->post_content = preg_replace( '#\[nggallery[^\]]*\]#i', $gallery, $post->post_content );
-		$post->post_content = apply_filters( 'engg_post_content', $post->post_content, $pristine_content, $attr, $post, $gallery );
-		wp_update_post( $post );
-		$this->posts_count++;
-		$this->infos[] = sprintf( "Updated post %d", $post->ID );	
+			$this->posts_count++;
 	}
-
 	/**
 	 * @param int $limit How many posts to get
 	 *
@@ -264,9 +256,9 @@ class Escape_NextGen_Gallery {
 			'fields'      => 'ids',
 			'posts_per_page' => $limit
 		);
-		
+
 		$args = apply_filters( 'escape_ngg_query_args', $args );
-		
+
 		$query = new WP_Query( $args );
 		return $query->posts;
 	}
